68cd3f9693fd46f1ef0066db172f94ead8e6f50a
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-11 15:12:51 -0600

    Update comment.routes.js

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -1,1 +1,11 @@
 import {Router} from 'express'
+
+import {
+    addComment,
+    getComment,
+    getCommentByPublication,
+    deleteComment,
+    updateComment   
+} from './comment.controller.js'
+
+q
