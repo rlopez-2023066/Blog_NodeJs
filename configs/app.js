f04972d929bcba52f0763e90da6297fc14193b27
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-10 21:32:50 -0600

    Create app.js

---
+++ configs/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+import publicationRoutes from './../src/publication/publication.routes.js'
+
+const configs = (app) => {
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+const routes = (app) => {
+    app.use('/v1/publication', publicationRoutes)
+}
+
+export const initServer = () => {
+    const app = express()
+    try{
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Servidor ejecutándose en el puerto ${process.env.PORT}`) 
+
+    }catch (error){
+        
+        console.error('Error en el Servidor', error)
+    }
+}
