665357c8357b5bbaf3e11aaa21d7d6fb80af2e87
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-12 09:09:15 -0600

    update validators.js

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -9,7 +9,8 @@ import{
 
 import {
     notRequiredField,
-    validateExistPublication
+    validateExistPublication,
+    validateExistComment
 } from './../utils/db.validator.js'
 
 export const validateRegisterPublication = [ 
@@ -41,7 +42,7 @@ export const validateUpdatePublication = [
     body('id')
     .notEmpty()
     .custom(async (id) => {
-            await validateExistPublication(id); // Llama a la función de validación
+            await validateExistPublication(id); 
         }),
     body('title')
     .optional()
@@ -68,6 +69,79 @@ export const validateUpdatePublication = [
     .notEmpty()
     .custom(notRequiredField),
 
+    validateErrorsWithoutFiles
+
+]
+
+export const validateRegisterComment = [
+    body('name')
+    .notEmpty()
+    .withMessage('You cant leave the name empty'),
+
+    body('content')
+    .notEmpty()
+    .withMessage('You cant leave the content empty'),
+
+    body('publicationId')
+    .notEmpty()
+    .withMessage('You cant leave the publicationId empty')
+    .custom(async (publicationId) => {
+            await validateExistPublication(publicationId); 
+    }),
+    
+    validateErrors
+]
+
+export const validateCommentByPublication = [
+    body('idPublication')
+    .notEmpty()
+    .withMessage('You cant leave the idPublication empty')
+    .custom(async (idPublication) => {
+            await validateExistPublication(idPublication); 
+    }),
+
+    validateErrors
+]
+
+export const validateDeleteComment = [
+    body('id')
+    .notEmpty()
+    .withMessage('You cant leave the ID empty')
+    .custom(async (id) => {
+            await validateExistComment(id); 
+    }),
+
+    validateErrors
+]
+
+export const validateUpdateComment = [
+    
+    body('id')
+    .notEmpty()
+    .withMessage('ID cannot be empty')
+    .custom(async (id) => {
+            await validateExistComment(id); 
+    }),
+
+    body('name')
+    .optional()
+    .notEmpty()
+    .withMessage('Name cannot be empty'),
+
+    body('content')
+    .optional()
+    .notEmpty()
+    .withMessage('Content cannot be empty'),
+
+    body('date')
+    .optional()
+    .custom(notRequiredField),
+    
+    body('publication')
+    .optional()
+    .custom(notRequiredField),
+
     validateErrors
 
 ]
+
